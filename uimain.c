6102e5ef98a6ebe06d48841a98781f9a7a9378e8
mennymen <mrmelendezlujan@miners.utep.edu>
2020-09-05 18:34:01 -0600

    Added the other two fonts

--- uimain.c
+++ uimain.c
@@ -7,7 +7,7 @@ int main()
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a ,Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Arrow = a ,Chars = c), Manuel (5x7 font) = 'm', Manuel (8x12 font) = 'M' ,or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -26,8 +26,26 @@ int main()
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'A'; c < 'D'; c++)
+      for (char c = 'A'; c < 'G'; c++)
 	print_char_11x16(c);
+      break;
+    case 'm':
+      puts("You selected Manuel (5x7 font):");
+      print_char_5x7('M');
+      print_char_5x7('a');
+      print_char_5x7('n');
+      print_char_5x7('u');
+      print_char_5x7('e');
+      print_char_5x7('l');
+      break;
+    case 'M':
+      puts("You selected Manuel (8x12 font):");
+      print_char_8x12('M');
+      print_char_8x12('a');
+      print_char_8x12('n');
+      print_char_8x12('u');
+      print_char_8x12('e');
+      print_char_8x12('l');
       break;
     case 'a':
       puts("You selected arrow:");
